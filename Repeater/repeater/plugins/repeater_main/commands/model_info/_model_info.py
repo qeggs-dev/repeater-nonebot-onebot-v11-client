318bafcb2ce8f603fd45d302ff0c47efa0b69602
Qeggs <get_boot@outlook.com>
2026-01-01 15:36:55 +0800

    refactor(repeater_main): 重命名变量以提高代码可读性
    - 将变量名 sendmsg 重命名为 send_msg，使其更清晰易懂
    - 更新所有相关的变量引用，包括 is_debug_mode 检查、
      send_debug_mode() 调用、send_error() 调用和
      send_check_length() 调用
    - 遵循 snake_case 命名规范，提升代码一致性

--- Repeater/repeater/plugins/repeater_main/commands/model_info/_model_info.py
+++ Repeater/repeater/plugins/repeater_main/commands/model_info/_model_info.py
@@ -13,24 +13,24 @@ get_model_list = on_command("getModelList", aliases={"gml", "get_model_list", "G
 @get_model_list.handle()
 async def handle_get_model_list(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
     persona_info = PersonaInfo(bot=bot, event=event, args=args)
-    sendmsg = SendMsg("Model.Get_Model_List", get_model_list, persona_info)
+    send_msg = SendMsg("Model.Get_Model_List", get_model_list, persona_info)
 
-    if sendmsg.is_debug_mode:
-        await sendmsg.send_debug_mode()
+    if send_msg.is_debug_mode:
+        await send_msg.send_debug_mode()
     else:
         model_info_core = ModelInfoCore()
         model_type_str = persona_info.message_str.strip()
         if model_type_str in MODEL_TYPES:
             model_type = ModelType(model_type_str)
         else:
-            await sendmsg.send_error("Not a valid model type")
+            await send_msg.send_error("Not a valid model type")
 
         response = await model_info_core.get_model_list(model_type)
         if response.code == 200:
             if response.data is not None:
-                await sendmsg.send_check_length("\n".join(response.data))
+                await send_msg.send_check_length("\n".join(response.data))
             else:
-                await sendmsg.send_error("Error: No Model Data")
+                await send_msg.send_error("Error: No Model Data")
         else:
-            await sendmsg.send_response(response)
+            await send_msg.send_response(response)
 
