626a0dd1443174a131c8cdc4697b844217a3f7de
Qeggs <get_boot@outlook.com>
2026-01-18 02:10:50 +0800

    feat(mixed): 添加会话分支克隆和绑定命令功能
    - 新增 handle_session_branch_clone 处理函数用于会话分支克隆
    - 新增 handle_session_branch_clone_from 处理函数用于从指定源克隆会话分支
    - 新增 handle_session_branch_bind 处理函数用于会话分支绑定
    - 新增 handle_session_branch_bind_from 处理函数用于从指定源绑定会话分支
    - 在 __init__.py 中注册新的处理函数到导出列表
    - 实现了四个新的命令处理器，支持多种别名形式

--- Repeater/repeater/plugins/repeater_main/commands/mixed/__init__.py
+++ Repeater/repeater/plugins/repeater_main/commands/mixed/__init__.py
@@ -1,4 +1,8 @@
 from ._branch_command.change_session import handle_change_session
+from ._branch_command.session_clone import handle_session_branch_clone
+from ._branch_command.session_clone_from import handle_session_branch_clone_from
+from ._branch_command.session_bind import handle_session_branch_bind
+from ._branch_command.session_bind_from import handle_session_branch_bind_from
 from ._branch_command.del_session import handle_delete_session
 
 from ._data_command._generate_prompt import handle_generate_prompt
@@ -6,5 +10,9 @@ from ._data_command._generate_prompt import handle_generate_prompt
 __all__ = [
     "handle_change_session",
     "handle_delete_session",
+    "handle_session_branch_clone",
+    "handle_session_branch_clone_from",
+    "handle_session_branch_bind",
+    "handle_session_branch_bind_from",
     "handle_generate_prompt"
 ]
