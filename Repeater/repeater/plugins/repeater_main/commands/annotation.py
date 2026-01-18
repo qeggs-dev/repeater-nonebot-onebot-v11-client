1a8ed57e8e5597d6b03ac46bf4d934cb13b97c83
Qeggs <get_boot@outlook.com>
2026-01-18 22:40:32 +0800

    feat(annotation): 添加 "/" 别名支持
    为注释命令添加 "/" 作为别名，用户现在可以使用 "#" 或 "/" 来触发注释功能，
    增加了使用的灵活性。

--- Repeater/repeater/plugins/repeater_main/commands/annotation.py
+++ Repeater/repeater/plugins/repeater_main/commands/annotation.py
@@ -3,7 +3,7 @@ from nonebot.rule import to_me
 from nonebot.adapters.onebot.v11 import Bot, MessageEvent, Message
 from nonebot.params import CommandArg
 
-comment = on_command("#", aliases={"anot", "annotation", "Annotation"}, rule=to_me(), block=True)
+comment = on_command("#", aliases={"/", "anot", "annotation", "Annotation"}, rule=to_me(), block=True)
 
 @comment.handle()
 async def handle_annotation(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
