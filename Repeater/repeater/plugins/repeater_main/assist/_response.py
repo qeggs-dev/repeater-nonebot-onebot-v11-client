e3d13e7eeaa67330fa14906984ab9ca2dcd0c16a
Qeggs <get_boot@outlook.com>
2026-01-23 16:10:28 +0800

    feat(repeater): 添加错误响应处理功能
    - 导入ErrorResponse类用于处理错误响应
    - 新增get_error方法，当响应码不为200时返回ErrorResponse对象
    - 增强Response类的功能，支持错误情况下的响应解析

--- Repeater/repeater/plugins/repeater_main/assist/_response.py
+++ Repeater/repeater/plugins/repeater_main/assist/_response.py
@@ -1,5 +1,6 @@
 from typing import Generic, TypeVar, Type, Any
 from httpx import Response as HTTPXResponse
+from ._error_response import ErrorResponse
 from pydantic import BaseModel
 
 T_Response = TypeVar("T_Response")
@@ -32,6 +33,10 @@ class Response(Generic[T_Response]):
         if issubclass(self._model, BaseModel):
             data: Any = self.json()
             return self._model(**data)
+    
+    def get_error(self) -> ErrorResponse | None:
+        if self.code != 200:
+            return ErrorResponse(**self.json())
 
     def __bool__(self) -> bool:
         return self.code == 200
