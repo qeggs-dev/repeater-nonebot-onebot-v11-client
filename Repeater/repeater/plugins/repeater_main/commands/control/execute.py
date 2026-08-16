de2c3ddf6e696bc21cc334116dbb95cddaa42d1f
Qeggs <get_boot@outlook.com>
2026-08-16 21:07:23 +0800

    feat(repeater_main): 新增execute命令用于通过组件调用执行命令
    新增execute命令(/execute或/e)，支持通过组件 component 直接调用命令执行。
    该功能允许在只知道命令 components 的情况下拉起命令调用，提供更灵活的命令执行方式。
    
    Usage: /execute components args

---
+++ Repeater/repeater/plugins/repeater_main/commands/control/execute.py
@@ -0,0 +1,69 @@
+import re
+import asyncio
+
+from typing import Any, Type
+from nonebot.adapters.onebot.v11 import Message
+from ...assist import PersonaInfo, SendMsg
+from ...cmd_info import CmdTypes
+from ...command_register import(
+    CommandCaller,
+    CommandPackage
+)
+
+@CommandCaller.register
+class Execute(CommandPackage):
+    cmd = "execute"
+    aliases = {
+        "e",
+        "E",
+        "Execute",
+        "EXECUTE"
+    }
+    cmd_type = CmdTypes.CONTROL
+    documents = f"""
+        Pull Up the command invocation through components when only the components of the command are known.
+
+        Usage:
+            /{cmd} components args
+    """
+
+    pattern = re.compile(r"^(?P<components>[\w\.]+)\s*(?P<args>.*)$", re.IGNORECASE | re.DOTALL | re.UNICODE)
+
+    async def handler(self, persona_info: PersonaInfo, send_msg: SendMsg):
+        msg = str(persona_info.message)
+        
+        matched = self.pattern.match(msg)
+        if matched:
+            components = matched.group("components")
+            args_prefix = matched.group("args")
+
+            assert isinstance(components, str), "components must be str"
+            assert isinstance(args_prefix, str), "args_prefix must be str"
+
+            args = Message(args_prefix)
+
+            try:
+                package = CommandCaller.match_component(components)
+            except KeyError:
+                await send_msg.send_error(f"Command {components} not found")
+                return
+            
+            try:
+                package_instance = CommandCaller.get_instance(package)
+            except KeyError as e:
+                await send_msg.send_error(f"Command instance {package.component} not found: {e}")
+                return
+
+            copyed_persona_info = persona_info.copy_with_args(
+                args = args
+            )
+            copyed_send_msg = send_msg.copy_with_component(
+                package_instance.component
+            )
+            await CommandCaller.horizontal_call(
+                package_instance,
+                copyed_persona_info,
+                copyed_send_msg
+            )
+        else:
+            await send_msg.send_error("Invalid command format")
