822ac49615a3755e708174bdc9e2737a173c0ec5
Qeggs <get_boot@outlook.com>
2026-08-19 05:05:42 +0800

    feat(Repeater): 添加CQ码消息发送命令
    新增SendMessageCQ命令用于发送CQ码消息，
    支持多种别名如smsgcq、send_message_cq等，
    需要超级用户权限且可通过配置控制是否允许发送任意消息

---
+++ Repeater/repeater/plugins/repeater_main/commands/send_msg/send_msg_cq.py
@@ -0,0 +1,33 @@
+from nonebot.adapters.onebot.v11 import Message
+
+from ...assist import PersonaInfo, SendMsg
+from ...cmd_info import CmdTypes
+from ...command_register import(
+    CommandCaller,
+    CommandPackage
+)
+from ...client_configs import storage_configs
+
+
+@CommandCaller.register
+class SendMessageCQ(CommandPackage):
+    cmd = "sendMessageCQ"
+    aliases = {
+        "smsgcq",
+        "SMSGCQ",
+        "send_message_cq",
+        "Send_Message_CQ",
+        "SendMessageCQ",
+        "SEND_MESSAGE_CQ",
+    }
+    cmd_type = CmdTypes.SENDMSG
+    superuser_permissions = True
+
+    async def handler(self, persona_info: PersonaInfo, send_msg: SendMsg):
+        if not storage_configs.allow_send_any_message:
+            await send_msg.send_error("Send_Message is disabled")
+            return
+
+        message = Message(persona_info.message_stripped_str)
+
+        await send_msg.send_any(message, reply=False)
