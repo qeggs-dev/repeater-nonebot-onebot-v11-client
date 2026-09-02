26760031ceba6725ebd94bf1f78447f691fded96
Qeggs <get_boot@outlook.com>
2026-09-02 11:37:44 +0800

    fix(persona_info): 修复 PersonaInfo 类中的参数传递方式
    将 make_message() 方法调用替换为 None，以防止 args 覆盖 message

--- Repeater/repeater/plugins/repeater_main/assist/persona_info/persona_info.py
+++ Repeater/repeater/plugins/repeater_main/assist/persona_info/persona_info.py
@@ -269,7 +269,7 @@ class PersonaInfo:
         async for event in self.get_reply_chain(max_depth):
             instance = self.copy(
                 event = event,
-                args = self.make_message(),
+                args = None,
                 copydata = copydata,
                 deepcopy = deepcopy
             )
